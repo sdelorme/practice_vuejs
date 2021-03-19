0818a69d7ac8fa7836645fe1d066e57a7cf36ce8
Seth Delorme <delorme.s.m@gmail.com>
2021-03-18 21:21:24 -0500

    creates computeSum in calculator.js

--- calculator.js
+++ calculator.js
@@ -2,7 +2,18 @@ var app = new Vue({
   el: "#app",
   data: function() {
     return {
-      message: "Hello Vue!"
+      message: "Input numbers below!",
+      number1: 0,
+      number2: 0,
+      number3: 0,
+      total: ""
     };
+  },
+  methods: {
+    computeSum: function () {
+      this.total = (parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3));
+      console.log(this.total); 
+    }
   }
-});
+}
+);
