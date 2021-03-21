a561522bab89fc26e29503ae72f176273a48129f
Seth Delorme <delorme.s.m@gmail.com>
2021-03-21 11:02:38 -0500

    adds addColor function to methods object in index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var app = new Vue({
   el: "#app",
   data: function() {
     return {
-      message: "Hello there,",
+      message: "Hello there",
       secret: "I like turtles",
       showSecret: false,
+      colors: ["red","blue","green","black","white"],
+      newColor: "",
     };
   },
   methods: {
@@ -15,6 +17,13 @@ var app = new Vue({
     toggleSecret: function () {
       console.log("workingggggggg");
       this.showSecret = !this.showSecret;
-    }
-  } 
+    },
+    showColors: function() {
+      console.log(this.colors);
+    },
+    addColor: function() {
+      this.colors.push(this.newColor);
+      this.newColor = "";
+    } 
+  }
 });
