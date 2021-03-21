1343ed48ea02501bac9a8ae126187e01c6486b98
Seth Delorme <delorme.s.m@gmail.com>
2021-03-21 10:04:19 -0500

    uses v-if to create toggle button to display message in index.html

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ var app = new Vue({
   data: function() {
     return {
       message: "Hello there,",
+      secret: "I like turtles",
+      showSecret: false,
     };
   },
   methods: {
     loudNoises: function (message) {
       this.message = "I DON'T KNOW WHAT WE'RE YELLING ABOUT!";
+    },
+    toggleSecret: function () {
+      console.log("workingggggggg");
+      this.showSecret = !this.showSecret;
     }
   } 
 });
